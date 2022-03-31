39763aedeb73f8aca7367413c1a231b593048bcf
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-03-31 13:58:25 +0200

    ajout de la save de la liste des todos dans l'app

--- intro-react/src/App.js
+++ intro-react/src/App.js
@@ -1,12 +1,25 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Header from "./Header";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   //const qui permet de chercher la valeur(ref) de l'input
   const todoNameRef = useRef();
+
+  //save de la liste des todos dans l'App (refresh page)
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (setTodos) setTodos(storedTodos);
+  }, []);
+
+  //save de la liste des todos en LOCAL
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   //function qui ajoute un todo à la liste
   function handleAddTodo(e) {
@@ -16,7 +29,7 @@ function App() {
       return [
         ...previousTodos,
         {
-          id: uuidv4(),
+          id: uuidv4(), //génère un id avec uuid
           name: name,
           complete: false,
         },
