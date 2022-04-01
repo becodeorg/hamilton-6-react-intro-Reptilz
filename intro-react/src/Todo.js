b426f7d999f8cfd802d155d1e537017e88acba35
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-04-01 14:38:07 +0200

    Ajout d'un fond plus foncé quand un todo est selectionné

--- intro-react/src/Todo.js
+++ intro-react/src/Todo.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo({ todo, toggleTodo }) {
+  const [style, setStyle] = useState("todo");
+
   //récupère l'id et l'envoie dans le paramètre de la fonction toggleTodo()
   function handleTodoClick() {
     toggleTodo(todo.id);
   }
+
+  //change la couleur du backgroud si une checkbox est cochée
+  function handleChangeStyle() {
+    if (todo.complete === false) {
+      setStyle("newStyle");
+    } else {
+      setStyle("todo");
+    }
+  }
   return (
-    <div>
+    <div className={style}>
       <label>
         <input
           type="checkbox"
           checked={todo.complete}
           onChange={handleTodoClick}
+          onClick={handleChangeStyle}
         />
         {todo.name}
       </label>
